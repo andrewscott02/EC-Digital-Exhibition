612cd9cbe23b8c4f85111eb9fa8493d05a03e6ee
AJ Scott <as243879@falmouth.ac.uk>
2023-02-24 13:46:35 +0000

    chore: Project Setup
    -New scene

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    public GameObject capsule;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (capsule != null)
+            capsule.SetActive(false);
     }
 
     // Update is called once per frame
