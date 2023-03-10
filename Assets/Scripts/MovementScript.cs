3ba5d5bae56d5d0b02a5f5607ec1edf8c0fd32d6
AJScott <as243879@falmouth.ac.uk>
2023-03-10 12:31:09 +0000

    feat: Player movement and mouse look

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    Rigidbody rb;
     public GameObject capsule;
+    public bool visibleOnStart = true;
+
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         if (capsule != null)
-            capsule.SetActive(false);
+            capsule.SetActive(visibleOnStart);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveVector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Move forwards
+            moveVector += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVector -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVector += transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVector -= transform.right;
+        }
+
+        rb.AddForce(moveVector * speed, ForceMode.Force);
     }
 }
